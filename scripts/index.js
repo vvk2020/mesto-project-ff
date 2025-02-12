3678351d5d61085cbbd783412d9c108687bab2d0
V Kuznetsov <vvsmith13@gmail.com>
2025-02-12 21:22:13 +0300

    rc1.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector(".places__list"); // контейнер карточек
 
 // @todo: Функция создания карточки
+createPlaceCard = (card) => {
+  // Клонирование карточки по шаблону
+  const newPlaceCard = cardTemplate
+    .querySelector(".places__item.card")
+    .cloneNode(true);
+  // Заполнение данными клона карточки
+  newPlaceCard.querySelector(".card__image").src = card.link;
+  newPlaceCard.querySelector(".card__title").textContent = card.name;
+  // Обработчик удаления карточки
+  const deleteButton = newPlaceCard.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => {
+    newPlaceCard.remove();
+  });
+  return newPlaceCard;
+};
 
 // @todo: Функция удаления карточки
 
+// FIXME: нужна ли отдельная функция ?
+
 // @todo: Вывести карточки на страницу
+appendCards = (cards, cardList) => {
+  cards.forEach((card) => {
+    cardList.append(createPlaceCard(card));
+  });
+};
+
+appendCards(initialCards, placesList);

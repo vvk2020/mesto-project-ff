1025d18e597f9d77b29383083e22c004777e82d6
V Kuznetsov <vvsmith13@gmail.com>
2025-03-11 14:53:28 +0300

    pw06-rc1.7b

--- src/components/modal.js
+++ src/components/modal.js
@@ -6,7 +6,7 @@
 */
 
 class Popup {
-  // Статические константы класса
+  //* Статические константы класса
   static CONST = {
     isOpenClass: "popup_is-opened", // css-класс открытого окна
     closerClass: ".popup__close", // css-класс элемента (кнопки), закрывающего окно
@@ -24,49 +24,66 @@ class Popup {
         this.buttonOpen = buttonOpen;
         this.attachEvent("click", this.openPopup, this.buttonOpen);
       }
-      /* popup открывается сразу после создания, если установлен флаг openIt или не задан элемент,
-      его открывающий (buttonOpen). Promise используется для выполнения initializePopup() строго 
-      после завершения работы данного конструктора */
-      if (openIt || !buttonOpen)
-        this.openPopup().then(this.initializePopup.bind(this));
+      /* Если установлен флаг openIt, то popup открывается сразу после создания. Promise необходим 
+      для выполнения initializePopup() строго после завершения работы данного конструктора */
+      if (openIt) this.openPopup().then(this.initializePopup.bind(this));
+      // Форма на popup (если есть) и ее submit-обработчик
+      this.form = this.popup.querySelector("form.popup__form");
+      if (this.form) {
+        this.form.addEventListener("submit", (evt) => {
+          evt.preventDefault(); // блокировка стандартной обработки submit
+          this.handleFormSubmit(evt); // подготовка данных формы popup для отправки
+          this.popup.classList.remove(Popup.CONST.isOpenClass); // скрытие popup
+        });
+      }
     }
   }
 
-  // Универсальная функция назначения обработчика событий
+  //* submit-обработчик формы popup (переопределяется в наследниках)
+  // Не удалять! Иначе не корректно работает в наследниках
+  handleFormSubmit(evt) {
+    // console.log("Popup.handleFormSubmit()");
+  }
+
+  //* Универсальная функция назначения обработчика событий
   attachEvent(event, handler, obj = this.popup) {
     if (obj && event && handler)
       obj.addEventListener(event, handler.bind(this));
   }
 
-  // Универсальная функция удаления обработчика событий
+  //* Универсальная функция удаления обработчика событий
   detachEvent(event, handler, obj = this.popup) {
     if (obj && event && handler)
       obj.removeEventListener(event, handler.bind(this));
   }
 
-  // Назначение обработчиков событий закрытия модального окна
+  //* Назначение обработчиков событий закрытия модального окна
   attachAllEvents() {
     this.attachEvent("click", this.closePopup, this.buttonClose); // по ❌
     this.attachEvent("keydown", this.handleEsc, document); // по Esc
     this.attachEvent("click", this.handleClickOutside); // click вне границ окна
   }
 
-  // Удаление обработчиков событий модального окна перед закрытием
+  //* Удаление обработчиков событий модального окна перед закрытием
   detachAllEvents() {
     this.detachEvent("click", this.closePopup, this.buttonClose); // по ❌
     this.detachEvent("keydown", this.handleEsc, document); // по Esc
     this.detachEvent("click", this.handleClickOutside); // click вне границ окна
   }
 
-  // Инициализация popup (определяется в наследниках])
+  //* Инициализация popup (определяется в наследниках)
   initializePopup() {
-    console.log("Popup.initializePopupForm()");
+    // console.log("Popup.initializePopupForm()");
   }
 
-  // Обработчик открытия модального окна по ✏️
+  //* Завершающие операции перед закрытием popup (определяется в наследниках)
+  finalizePopup() {
+    console.log("Popup.finalizePopup()");
+  }
+
+  //* Обработчик открытия модального окна по ✏️
   async openPopup() {
     this.popup.classList.add(Popup.CONST.isOpenClass);
-    this.attachAllEvents(); // назначение обработчиков событий окна
     /* Если popup неоходимо открыть сразу после конструктора (this.openIt===true), то используем Promise,
     если нет - вызываем как обычно */
     if (this.openIt) {
@@ -74,53 +91,54 @@ class Popup {
     } else this.initializePopup();
   }
 
-  // Обработчик закрытия модального окна по ❌
+  //* Обработчик закрытия модального окна по ❌
   closePopup() {
     /* Удаление обработчиков событий окна с блокировкой повторного их удаления */
     if (this.popup.classList.contains(Popup.CONST.isOpenClass))
       this.detachAllEvents();
     // Удаление флага открытия окна (налияия обработчиков событий)
     this.popup.classList.remove(Popup.CONST.isOpenClass);
+    this.finalizePopup();
   }
 
-  // Обработчик закрытия модального окна по Esc
+  //* Обработчик закрытия модального окна по Esc
   handleEsc(evt) {
     if (evt.key === "Escape") {
       this.closePopup();
     }
   }
 
-  // Обработчик закрытия модального окна по click вне его границ
+  //* Обработчик закрытия модального окна по click вне его границ
   handleClickOutside(evt) {
     const isInsideClick = !!evt.target.closest(Popup.CONST.contentWrapperClass);
     // Закрываем, если click по элементу, не имеющему родителя с .popup__content
     if (!isInsideClick) this.closePopup();
   }
 
-  // Подготовка данных формы popup (в т.ч. перед отправкой на сервер)
+  //* Подготовка данных формы popup (в т.ч. перед отправкой на сервер)
   // https://doka.guide/js/deal-with-forms/
-  // https://learn.javascript.ru/formdata => fformData.get(name) – получает значение поля с именем name,
-  // serializeForm(formNode) {
-  //   const { elements } = formNode;
-  //   const data = new FormData(); // данные формы для отправки на сервер
-
-  //   Array.from(elements)
-  //     .filter((item) => !!item.name)
-  //     .forEach((element) => {
-  //       const { name, type } = element;
-  //       const value = type === "checkbox" ? element.checked : element.value; // поддержка checkbox
-
-  //       data.append(name, value);
-  //     });
-  //   return data;
-  // }
+  // https://learn.javascript.ru/formdata => formData.get(name) – получает значение поля с именем name,
+  serializeForm() {
+    if (this.form) {
+      const { elements } = this.form;
+      this.data = new FormData(); // данные формы для отправки на сервер
+      Array.from(elements)
+        .filter((item) => !!item.name)
+        .forEach((element) => {
+          const { name, type } = element;
+          const value = type === "checkbox" ? element.checked : element.value; // поддержка checkbox
+          this.data.append(name, value);
+        });
+      // console.log(Array.from(this.data.entries()));
+    }
+  }
 }
 
-// Класс popup редактирования профиля
+//! Класс popup редактирования профиля
 class ProfilePopup extends Popup {
-  // Статические константы класса
+  //* Статические константы класса
   static CONST = {
-    /******** CSS-классы *******/
+    //* CSS-классы
     popupClass: ".popup_type_edit", // popup
     //... полей формы popup
     profileNameClass: ".popup__input_type_name", // имени профиля
@@ -128,18 +146,20 @@ class ProfilePopup extends Popup {
   };
 
   constructor(buttonOpen, OutputFields, openIt = false) {
-    /*  buttonOpen - кнопка открытия модального окна
-        OutputFields -  объект ссылок на элементы профия
-        openIt - флаг необходимости открытия модального окна: true - открыть, false - создать */
+    /**  
+      @param buttonOpen - кнопка открытия модального окна
+      @param OutputFields -  объект ссылок на элементы профия
+      @param openIt - флаг необходимости открытия модального окна: true - открыть, false - создать 
+    */
     const popup = document.querySelector(ProfilePopup.CONST.popupClass);
     if (popup) {
       super(popup, buttonOpen, openIt);
 
-      /******* Поля формы popup *******/
+      //* Поля формы popup
       this.profileName = popup.querySelector(
         ProfilePopup.CONST.profileNameClass
       ); // Имя профиля
-      this.profileDescriptionClass = popup.querySelector(
+      this.profileDescription = popup.querySelector(
         ProfilePopup.CONST.profileDescriptionClass
       ); // Описание профиля
       // Элементы страницы, отображающие данные профиля
@@ -147,33 +167,41 @@ class ProfilePopup extends Popup {
     }
   }
 
+  //* Инициализация popup (определяется в наследниках)
   initializePopup() {
-    /******* Передача данных в поля формы popup *******/
+    // Передача данных в поля формы popup
     if (this.OutputFields) {
       // Имя профиля
       if (this.OutputFields.name.textContent) {
-        this.profileName = this.popup.querySelector(
-          ProfilePopup.CONST.profileNameClass
-        );
         this.profileName.value = this.OutputFields.name.textContent;
       }
       // Описание профиля
       if (this.OutputFields.description.textContent) {
-        this.profileDescription = this.popup.querySelector(
-          ProfilePopup.CONST.profileDescriptionClass
-        );
         this.profileDescription.value =
           this.OutputFields.description.textContent;
       }
     }
   }
+
+  //* Завершающие операции перед закрытием popup
+  handleFormSubmit(evt) {
+    super.handleFormSubmit(evt);
+    console.log("ProfilePopup.handleFormSubmit().this", this);
+    this.serializeForm(); // подготовка данных для отправки на сервер
+    // Передача данных из popup профиля на страницу
+    if (this.OutputFields && this.data) {
+      console.log("ProfilePopup.finalizePopup()+++");
+      this.OutputFields.name.textContent = this.data.get("name"); // имя
+      this.OutputFields.description.textContent = this.data.get("description"); // описание
+    }
+  }
 }
 
-// Класс popup отображения выбранной карточки
+//! Класс popup отображения выбранной карточки
 class CardPopup extends Popup {
   // Статические константы класса
   static CONST = {
-    /******** CSS-классы *******/
+    // * CSS-классы
     // ... popup и его элементов:
     popupClass: ".popup_type_image", // popup
     popupImageClass: ".popup__image", // картинки
@@ -184,19 +212,21 @@ class CardPopup extends Popup {
   };
 
   constructor(Card, openIt = false) {
-    /*  Card - ссылка на выбранную карточку
-        openIt - флаг необходимости открытия модального окна: true - открыть, false - создать */
+    /**  
+      @param Card - ссылка на выбранную карточку
+      @param openIt - флаг необходимости открытия модального окна: true - открыть, false - создать
+    */
     const popup = document.querySelector(CardPopup.CONST.popupClass);
     if (popup) {
       super(popup, null, openIt);
-      /******* Элементы popup *******/
+      //* Элементы popup
       this.imagePopup = this.popup.querySelector(
         CardPopup.CONST.popupImageClass
       ); // картинка
       this.captionPopup = this.popup.querySelector(
         CardPopup.CONST.popupCaptionClass
       ); // описание
-      /******* Элементы карточки *******/
+      //* Элементы карточки
       if (Card) {
         this.Card = Card;
         this.imageCard = this.Card.querySelector(
@@ -209,7 +239,7 @@ class CardPopup extends Popup {
     }
   }
 
-  // Передача данных элементам popup из карточки
+  //* Передача данных элементам popup из карточки
   initializePopup() {
     if (this.Card) {
       if (this.imageCard && this.imagePopup) {

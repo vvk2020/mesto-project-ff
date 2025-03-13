fab27031ea337b92ab2584504b5ab749748cf631
V Kuznetsov <vvsmith13@gmail.com>
2025-03-13 21:58:52 +0300

    pw06-rc1.10

--- src/components/card.js
+++ src/components/card.js
@@ -1,27 +1,42 @@
-//* Темплейт карточки
+//* Шаблон карточки
 const cardTemplate = document.querySelector("#card-template").content;
 
 //* Функция создания карточки
-const createCard = (card, handler = deleteCard) => {
+const createCard = (card, onShow, onDelete = deleteCard) => {
   // Клонирование карточки по шаблону
   const newCard = cardTemplate
     .querySelector(".places__item.card")
     .cloneNode(true);
   // Если клон успешно создан, то его инициализация
   if (newCard) {
-    // Данные карточки
+    // Картинка карточки
     const cardImage = newCard.querySelector(".card__image");
+
     if (cardImage) {
+      // Данные карточки
       cardImage.src = card.link;
       cardImage.alt = card.name;
+      // Обработчик открытия popup просмотра карточки
+      cardImage.addEventListener("click", () => {
+        onShow(card); // открываем popup
+      });
     }
+
     newCard.querySelector(".card__title").textContent = card.name;
-    // Обработчик удаления карточки
+
+    // Обработчик удаления карточки (автоматически удаляется после первого срабатывания)
     const deleteButton = newCard.querySelector(".card__delete-button");
     if (deleteButton)
-      deleteButton.addEventListener("click", () => {
-        handler(newCard);
-      });
+      deleteButton.addEventListener(
+        "click",
+        () => {
+          onDelete(newCard);
+        },
+        { once: true }
+      );
+
+    //
+    // const likeButton
   }
   return newCard;
 };
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,33 +1,74 @@
 import initialCards from "../components/cards.js";
-import { createCard, likeCard } from "../components/card.js";
-import {
-  ProfilePopup,
-  CardViewPopup,
-  CardAddPopup,
-} from "../components/modal.js";
+import { createCard, deleteCard, likeCard } from "../components/card.js";
+import { openModal } from "../components/modal.js";
 import "../pages/index.css";
+
+/*********************************************************************************************/
 
 //!  DOM узлы
 const cardsContainer = document.querySelector(".places__list"); // контейнер карточек
-//* popup'ов
-const profilePopup = new ProfilePopup(); // редактирования профиля
-const cardAddPopup = new CardAddPopup(); // добавления новой карточки
-const cardViewPopup = new CardViewPopup(); // просмотра карточки с описанием
-//* Кнопки, открывающие popup'ы
-const buttonEditProfile = document.querySelector(".profile__edit-button"); // редактирования профиля
-const buttonAddCard = document.querySelector(".profile__add-button"); // добавления новой карточки
-//
+//* popup's
+// const profilePopup = document.querySelector(".popup.popup_type_edit"); // редактирования профиля
+// const cardAddPopup = document.querySelector(".popup.popup_type_new-card"); // создания карточки
+const cardViewPopup = document.querySelector(".popup.popup_type_image"); // просмотра карточки
+
+//* поля popup просмотра карточки
+const imageCardViewPopup = cardViewPopup.querySelector("img.popup__image");
+const captionCardViewPopup = cardViewPopup.querySelector(".popup__caption");
+
+//! Обработчики событий
+
+//* Обработчик отображения popup просмотра карточки
+function handleShowCard(card) {
+  setupCardViewPopup(card); // настройка popup
+  if (cardViewPopup) openModal(cardViewPopup); // открытие popup
+}
+
+//* Обработчик закрытия popup просмотра карточки
+function handleHideCard(card) {
+  // setupCardViewPopup(card); // настройка popup
+  if (cardViewPopup) closeModal(cardViewPopup); // открытие popup
+}
+
+//! Вспомогательные функции
+
+//* Настройка окна отображения карточки
+function setupCardViewPopup(card) {
+  // Трансфер данных из карточки в popup
+  if (card.name && card.link && imageCardViewPopup) {
+    // Картинка
+    if (imageCardViewPopup) {
+      imageCardViewPopup.src = card.link;
+      imageCardViewPopup.alt = card.name;
+    }
+    // Название
+    if (captionCardViewPopup) captionCardViewPopup.textContent = card.name;
+  }
+}
 
 //! Вывод лакльно сохраненных карточек на страницу из initialCards[]
 const appendCards = (cardList, ...cards) => {
   cards.forEach((card) => {
-    const newCard = createCard(card);
+    const newCard = createCard(card, handleShowCard);
     if (newCard) cardList.append(newCard);
   });
 };
 
+// // Инициализация popup'ов
+
 appendCards(cardsContainer, ...initialCards);
 
+/*********************************************************************************************/
+
+// //* popup'ов
+// const profilePopup = new ProfilePopup(); // редактирования профиля
+// const cardAddPopup = new CardAddPopup(); // добавления новой карточки
+// const cardViewPopup = new CardViewPopup(); // просмотра карточки с описанием
+// //* Кнопки, открывающие popup'ы
+// const buttonEditProfile = document.querySelector(".profile__edit-button"); // редактирования профиля
+// const buttonAddCard = document.querySelector(".profile__add-button"); // добавления новой карточки
+
+/*
 //! Назначение обработчиков ...
 
 //* ... открытия popup редактирования профиля
@@ -44,7 +85,7 @@ buttonAddCard.addEventListener("click", () => {
   cardAddPopup.openModal();
 }); // создания новой карточки
 
-//* ... открытия popup карточек и like/dislike 
+//* ... открытия popup карточек и like/dislike
 cardsContainer.addEventListener("click", (evt) => {
   // Открытие popup выбранной карточки
   if (evt.target.classList.contains("card__image")) {
@@ -58,3 +99,6 @@ cardsContainer.addEventListener("click", (evt) => {
     likeCard(evt.target); // изменение стиля 🩷
   }
 });
+*/
+
+// export { cardAddPopup };
